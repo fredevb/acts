16fb6a61ca7608bb32cb7791788edd4ac9f017ee
Frederik <frederik.barba@hotmail.com>
2024-04-22 09:39:07 +0200

    Notes

--- Plugins/Traccc/src/TracccConversion.cpp
+++ Plugins/Traccc/src/TracccConversion.cpp
@@ -9,6 +9,13 @@
 #include "Acts/Plugins/Traccc/TracccConversion.hpp"
 
 namespace Acts::TracccConversion{
+
+// https://github.com/acts-project/acts/blob/edd5661cdb0fb0f7e76848d196c626bf07c356e3/Examples/Python/python/acts/examples/reconstruction.py#L1807
+// https://github.com/acts-project/acts/blob/edd5661cdb0fb0f7e76848d196c626bf07c356e3/Examples/Python/src/Framework.cpp#L172
+
+// https://github.com/acts-project/traccc/blob/main/examples/run/cpu/seq_example.cpp
+
+// Test root field conversion to covfie.
 bool test(){
     return true;
 }
